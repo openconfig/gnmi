8a14ac0e9ed67e08988f9913243d89f398454824
awly <awly@google.com>
2017-09-05 11:22:23 -0700

    Use protos from github.com/google/protobuf
    github.com/golang/protobuf also has any.proto, but we should use the general
    one.
    
    Also, clean up compile_protos.sh to use proper import paths.
    
    PiperOrigin-RevId: 167605576

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -euo pipefail
+
+proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src"
 
 # Go
-protoc -I=testing/fake/proto:${GOPATH}/src --go_out=testing/fake/proto testing/fake/proto/fake.proto
-protoc -I=proto/gnmi:${GOPATH}/src --go_out=plugins=grpc:proto/gnmi --proto_path=${GOPATH}/src proto/gnmi/gnmi.proto
+protoc -I=$proto_imports --go_out=. testing/fake/proto/fake.proto
+protoc -I=$proto_imports --go_out=plugins=grpc:. proto/gnmi/gnmi.proto
 
 # Python
-python -m grpc_tools.protoc -I=testing/fake/proto:${GOPATH}/src --python_out=testing/fake/proto --grpc_python_out=testing/fake/proto testing/fake/proto/fake.proto
-python -m grpc_tools.protoc -I=proto/gnmi:${GOPATH}/src --python_out=proto/gnmi --grpc_python_out=proto/gnmi proto/gnmi/gnmi.proto
+python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. testing/fake/proto/fake.proto
+python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. proto/gnmi/gnmi.proto
