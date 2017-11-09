5254108ffb0645a6f8d59bd95844f4d07e10f35f
awly <awly@google.com>
2017-11-09 13:58:18 -0800

    Add a helper tool to generate fake.proto config
    This is a brute-ish way to help people create configs for fake_server (it's
    difficult to write valid text protos by hand).
    
    PiperOrigin-RevId: 175208103

--- testing/fake/gnmi/cmd/fake_server/README.md
+++ testing/fake/gnmi/cmd/fake_server/README.md
@@ -1,7 +1,10 @@
 ## Running the fake gNMI target
 
 First create a config file (text protobuf from `testing/fake/proto/fake.proto`).
-A simple example config can be found in `example-config.pb.txt`.
+
+You can use a `gen_fake_config` tool in this repo (modify
+`../gen_fake_config/gen_config.go` as needed and `go run
+../gen_fake_config/gen_config.go`).
 
 Next, compile the server binary (`go build` or `go install`).
 
@@ -10,11 +13,18 @@ Make sure you have TLS key pair for the server (a self-signed pair is fine).
 Finally, run:
 
 ```
-$ ./server --config=example-config.pb.txt --text --port=8080 --logtostderr --server_crt=<path to server TLS cert> --server_key=<path to server TLS key>
+$ fake_server --config config.pb.txt --text --port 8080 --server_crt <path to server TLS cert> --server_key <path to server TLS key> --allow_no_client_auth --logtostderr
 ```
 
 (replace binary and config paths as needed).
 
-You can skip client authentication with `--allow_no_client_auth`.
+If you wish to verify client TLS authentication, don't use
+`--allow_no_client_auth` and set `--ca_crt` to CA certificate path.
 
-Check the `./server --help` output for full flag set. TLS flags are described there.
+To verify your server is running, try:
+
+```
+$ gnmi_cli -a localhost:8080 -q '*' -logtostderr -insecure -qt s
+```
+
+Check the `./server --help` output for full flag set.
---
+++ testing/fake/gnmi/cmd/gen_fake_config/gen_config.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// The gen_fake_config command converts a hardcoded fake.proto message into a
+// textual protobuf. The source code is intended to be modified manually to
+// generate a valid text proto, instead of writing it by hand.
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	
+	log "github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
+
+	fpb "github.com/openconfig/gnmi/testing/fake/proto"
+)
+
+// Modify the config below to change generated output.
+var (
+	outputPath = "config.pb.txt"
+
+	config = &fpb.Config{
+		Target: "fake target name",
+		Seed:   12345,
+		Values: []*fpb.Value{
+			{
+				Path:   []string{"a", "b"},
+				Repeat: 3,
+				Value:  &fpb.Value_IntValue{&fpb.IntValue{Value: 4}},
+			},
+			{
+				Path:   []string{"b", "c"},
+				Repeat: 5,
+				Value:  &fpb.Value_StringValue{&fpb.StringValue{Value: "foo"}},
+			},
+		},
+		DisableSync: false,
+		DisableEof:  false,
+		EnableDelay: false,
+		ClientType:  fpb.Config_GRPC_GNMI,
+	}
+)
+
+func main() {
+	flag.Parse()
+	out := proto.MarshalTextString(config)
+	if err := ioutil.WriteFile(outputPath, []byte(out), os.ModePerm); err != nil {
+		log.Exit(err)
+	}
+}
