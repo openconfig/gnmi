0fbc892f6fd422410ed4d5d4147971dba97cd3a8
mkhsueh <mkhsueh@google.com>
2020-03-02 15:48:48 -0800

    Add new collector proto to gNMI compile script
    Use --go_out=plugins=grpc:. option as it contains a gRPC definition.
    
    PiperOrigin-RevId: 298467101

--- compile_protos.sh
+++ compile_protos.sh
@@ -23,9 +23,11 @@ protoc -I=$proto_imports --go_out=. testing/fake/proto/fake.proto
 protoc -I=$proto_imports --go_out=plugins=grpc:. proto/gnmi/gnmi.proto
 protoc -I=$proto_imports --go_out=plugins=grpc:. proto/gnmi_ext/gnmi_ext.proto
 protoc -I=$proto_imports --go_out=. proto/target/target.proto
+protoc -I=$proto_imports --go_out=plugins=grpc:. proto/collector/collector.proto
 
 # Python
 python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. testing/fake/proto/fake.proto
 python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. proto/gnmi_ext/gnmi_ext.proto
 python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. proto/gnmi_ext/gnmi_ext.proto
 python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. proto/target/target.proto
+python -m grpc_tools.protoc -I=$proto_imports --python_out=. --grpc_python_out=. proto/collector/collector.proto
