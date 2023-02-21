4a11050420ed9f7bd5a7f970134f2a2ccadfb855
n0shut <25195414+n0shut@users.noreply.github.com>
2023-02-21 13:48:45 +0100

    changed to new versioning scheme

--- compile-protos-docker.sh
+++ compile-protos-docker.sh
@@ -1,10 +1,14 @@
 # This script is used to generate Go and Python language bindings for the proto files in this repository.
-# It uses srl-labs/protoc:0.0.3 docker image to generate the bindings. The image contains the pinned versions of the
-# protoc compiler and the Go and Python GRPC plugins.
+# It uses ghcr.io/srl-labs/protoc container image to generate the bindings. See https://github.com/srl-labs/protoc-container for more details.
+# to generate all protos: ./compile-protos-docker.sh
+
+# container image tag, to see all available tags go to
+# https://github.com/srl-labs/protoc-container/pkgs/container/protoc
+IMAGE_TAG="21.12__1.28.1"
 
 DOCKER_CMD="docker run -v $(pwd):/in \
   -v $(pwd)/proto:/in/github.com/openconfig/gnmi/proto \
-  ghcr.io/srl-labs/protoc:0.0.3"
+  ghcr.io/srl-labs/protoc:${IMAGE_TAG}"
 
 PROTOC_GO_CMD='protoc -I ".:/ext_protos" --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false'
 
@@ -34,9 +38,9 @@ ${DOCKER_CMD} \
 ${DOCKER_CMD} \
   bash -c "${PROTOC_GO_CMD_NOGRPC} proto/target/target.proto"
 
-# #
-# ## Generating Python bindings ##
-# #
+#
+## Generating Python bindings ##
+#
 
 PROTOC_PY_CMD='python3 -m grpc_tools.protoc -I ".:/ext_protos" --python_out=. --grpc_python_out=.'
 
